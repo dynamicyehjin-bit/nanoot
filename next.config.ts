bb1425e5a55c58aadd7a2aa291a54e64ab0bec72
이우진 GitHub eunoia2.26 <dnwls2226@gmail.com>
2026-03-18 15:46:47 +0900

    fix: 이미지 도메인 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bdsaryrbyhlzpraakbpb.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
