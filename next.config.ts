75c08e0ed218db1856b349dbf6c79e847e9ca3ea
이우진 GitHub eunoia2.26 <dnwls2226@gmail.com>
2026-03-30 16:39:31 +0900

    feat: update participated and hosted co-buying UI

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bdsaryrbyhlzpraakbpb.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "k.kakaocdn.net",
-      },
-      {
-        protocol: "http",
-        hostname: "k.kakaocdn.net",
-      }
+      { hostname: 'i.pravatar.cc' },
+      { hostname: 'images.unsplash.com' },
+      { hostname: '*.supabase.co' },
     ],
   },
 };
